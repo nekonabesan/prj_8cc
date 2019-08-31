f2ff3ad3f9490ef412b96f39259f5b74e5f7f994
nekonabesan <sheepdogs_2@hotmail.com>
2019-08-31 16:35:18 +0900

    First Commit

---
+++ test/8cc_main_target.h
@@ -0,0 +1,17 @@
+// テスト対象関数のプロトタイプ宣言
+#ifndef _TARGET_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <string.h>
+
+#define _TARGET_H_
+#define RMAX 1000
+#define TRUE 1
+#define FALSE 0
+
+// 以下のプロトタイプ宣言
+
+#endif /* _TARGET_H_ */
