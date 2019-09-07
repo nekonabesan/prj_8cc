6b0e7d5027a9f4d0ac2d02dfc4fe4a7da3c2f94a
nekonabesan <sheepdogs_2@hotmail.com>
2019-09-07 16:27:07 +0900

    https://github.com/rui314/chibicc/commit/6ddba4be5f63388607fc77fd786267b9ddcb14c9

--- header.c
+++ header.c
@@ -8,31 +8,52 @@
 #include <string.h>
 
 
+//
+// Toakenizer
+//
+
 typedef enum {
   TK_RESERVED,
   TK_NUM,
   TK_EOF,
 } TokenKind;
 
+// Token type
 typedef struct Token Token;
+struct Token{
+  TokenKind kind;
+  Token *next;
+  int val;
+  char *str;
+  int len;
+};
+
+// Input program
+char *user_input;
+
+// Current token
+Token *token;
+
+//
+// Paser
+//
 
 typedef enum {
   ND_ADD, // +
   ND_SUB, // -
   ND_MUL, // *
   ND_DIV, // /
-  ND_NUM, // whole number
+  ND_EQ,  // ==
+  ND_NE,  // !=
+  ND_LT , // <
+  ND_LE,  // <=
+  ND_NUM, // Integer
 } NodeKind;
 
+//
+// As node type
+//
 typedef struct Node Node;
-
-struct Token{
-  TokenKind kind;
-  Token *next;
-  int val;
-  char *str;
-};
-
 struct Node{
   NodeKind kind;
   Node *lhs;
@@ -40,23 +61,24 @@ struct Node{
   int val;
 };
 
-Token *token;
-char *user_input;
-
 #define _TARGET_H_
 
 void error(char *fmt, ...);
 void error_at(char *loc, char *fmt, ...);
-bool consume(char op);
+bool consume(char *op);
+void expect(char *op);
 int expect_number();
 bool at_eof();
-Token *new_token(TokenKind kind, Token *cur, char *str);
+Token *new_token(TokenKind kind, Token *cur, char *str, int len);
+bool startswith(char *p, char *q);
 Token *tokenize();
 Node *new_node(NodeKind kind);
 Node *new_binary(NodeKind kind, Node *lhs, Node *rhs);
 Node *new_num(int val);
-Node *new_node_num(int val);
 Node *expr();
+Node *equality();
+Node *relational();
+Node *add();
 Node *mul();
 Node *primary();
 void gen(Node *node);
--- main.c
+++ main.c
@@ -1,24 +1,23 @@
 #include "header.c"
-#include "modules/9cc_error_module.c"
 #include "modules/9cc_module.c"
 
 
 int main(int argc, char **argv){
   if(argc != 2){
     error("%s: invalid number of argument", argv[0]);
-    return 1;
   }
 
-
+  // Tonkenize and parse
   user_input = argv[1];
   token = tokenize();
   Node *node = expr();
 
+  // Print out the first half of assembly
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
 
-  //
+  // Traver the AST to emmit assembly
   gen(node);
 
   printf("  pop rax\n");
